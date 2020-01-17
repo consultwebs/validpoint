84f8ce8093c6339e26ce7ad9aff541bf2f11c7e5
Chris Ostmo <costmo@consultwebs-email.com>
2020-01-17 09:58:55 -0800

    JSONify the responses for future functionality

--- classes/CW_Runner.js
+++ classes/CW_Runner.js
@@ -45,23 +45,48 @@ class CW_Runner
 		let async = require( "async" );
 		const CW_Network = require( "./CW_Network.js" );
 		const network = new CW_Network();
+
+		// TODO: populate the reponse object with values from a helper class instead if individual lines in each 'case'
+		let responseObject = 
+		{
+			test: "",
+			description: "",
+			result: "",
+			result_advice: ""
+		};
 		
 		switch( command )
 		{
 			case "local":
+				responseObject.test = "local-network";
+				responseObject.description = "Local network connectivity check";
+
 				network.checkLocalNetwork()
 					.then( 
 						( result ) => 
-						{ 
-							console.log( `LOCAL PING result: ${result}` );
+						{
+							responseObject.result = result;
+							if( result != "up" )
+							{
+								responseObject.result_advice = "Your local network is currently offline. Check your Internet connection and try again."
+							}
+							console.log( JSON.stringify( responseObject ) );
 						});
 				break;
 			case "dns":
+				responseObject.test = "local-dns";
+				responseObject.description = "Local DNS connectivity check";
+
 				network.checkDns()
 					.then( 
 						( result ) => 
 						{ 
-							console.log( `LOCAL DNS result: ${result}` );
+							responseObject.result = result;
+							if( result != "up" )
+							{
+								responseObject.result_advice = "Your local Domain Name Service (DNS) is not working. Contact your Internet Service Provider and try again.";
+							}
+							console.log( JSON.stringify( responseObject ) );
 						});
 				break;
 			case "http":
