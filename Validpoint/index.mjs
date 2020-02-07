0e1d53e724a47fe89eae569be5fed1e5c35da8fb
Chris Ostmo <costmo@consultwebs-email.com>
2020-02-06 19:48:58 -0800

    Moved command runner logic to it's own function to simplify exposed functions

--- Validpoint/index.mjs
+++ Validpoint/index.mjs
@@ -11,14 +11,28 @@ const runner = new CW_Runner();
 const CW_InputParser =  require( "../classes/CW_InputParser.js" );
 let config = null;
 
+exports.testHTTPPort = 
+( domain, callback ) =>
+{
+	commandRunner( { domain: domain, command: "http-port", callback: callback } );
+};
+
+exports.testHTTPSPort = 
+( domain, callback ) =>
+{
+	commandRunner( { domain: domain, command: "https-port", callback: callback } );
+};
+
 exports.jsonConfig = 
 () =>
 {
 	return "jsonConfig";
 };
 
-exports.testHTTPPort = 
-( domain, callback ) =>
+
+
+let commandRunner = 
+( { domain = null, command = null, callback = null } ) =>
 {
 	CW_InputParser.makeRunnerObjects( domain ).then(
 		( runObjects ) =>
@@ -43,53 +57,7 @@ exports.testHTTPPort =
 								let CW_Advice = require( "../classes/CW_Advice.js" );
 								let advice = new CW_Advice();
 
-								runner.runCommand( { command: "http-port", configObject: config, adviceObject: advice } )
-									.then(
-										( response ) =>
-										{
-											callback( null, response );
-										}
-									);
-							}
-							catch( exception )
-							{
-								// TODO: Handle exceptions
-								// console.log( exception );
-							}
-						}
-					);
-				}
-			);
-		});
-};
-
-exports.testHTTPSPort = 
-( domain, callback ) =>
-{
-	CW_InputParser.makeRunnerObjects( domain ).then(
-		( runObjects ) =>
-		{
-			runObjects.forEach(
-				runObject =>
-				{
-					let parser = new CW_InputParser( runObject.file, "./input/" );
-
-					parser.init(
-						async function()
-						{
-							try
-							{
-								config = this.parseJsonString();
-
-								if( config == null )
-								{
-									throw new Error( "COULD NOT READ INPUT CONFIGURATION FILE. EXITING." );
-								}
-
-								let CW_Advice = require( "../classes/CW_Advice.js" );
-								let advice = new CW_Advice();
-
-								runner.runCommand( { command: "https-port", configObject: config, adviceObject: advice } )
+								runner.runCommand( { command: command, configObject: config, adviceObject: advice } )
 									.then(
 										( response ) =>
 										{
